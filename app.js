7c9cfe202adf54184679f78d4276b0b379df684a
saddamseikh <saddamseikh2229@gmail.com>
2023-10-31 22:38:59 +0530

    Cors error resolve

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const express = require('express');
 const authRouter = require('./router/authRoute.js');
 const databaseconnect = require('./config/databaseConfig');
 const app = express(); // express instance create 
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 
 //database connect 
 databaseconnect();
 
 app.use(express.json())
 app.use(cookieParser());
-app.use('/api/auth/',authRouter)
+app.use('/api/auth/',authRouter);
+app.use(cors({
+    origin:[process.env.CLIENT_URL],
+    credentials: true,
+}))
 //basic route create 
 app.use('/', (req, res)=>{
     res.status(200).json({data:'JWT_server-server'});
